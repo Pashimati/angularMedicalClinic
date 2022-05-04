09572c9f7ae7956a0a8f41856e931d135274aea0
Pashimati <Pashimatii@gmail.com>
2022-05-04 22:31:18 +0300

    add header and footer components

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
+
+}
--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {IndexRoutingModule} from "./index-routing.module";
 import {IndexComponent} from "./index.component";
 import {OurDepartmentsComponent} from "./components/our-departments/our-departments.component";
 import {HomepageBoxesComponent} from "./components/homepage-boxes/homepage-boxes.component";
 import {SectionIndexComponent} from "./components/section-index/section-index.component";
 import {TheNewsComponent} from "./components/the-news/the-news.component";
-import {CommonModule} from "@angular/common";
-import {IndexRoutingModule} from "./index-routing.module";
+
 
 @NgModule({
   declarations: [
+    IndexComponent,
     HomepageBoxesComponent,
     OurDepartmentsComponent,
     SectionIndexComponent,
@@ -18,6 +20,7 @@ import {IndexRoutingModule} from "./index-routing.module";
     CommonModule,
     IndexRoutingModule
   ],
+  exports: [IndexComponent],
   providers: [],
   bootstrap: [IndexComponent]
 })
