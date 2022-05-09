f355d7f55a716267d9f9726c01328dc7e02d5b5f
Pashimati <Pashimatii@gmail.com>
2022-05-09 11:12:31 +0300

    add api service

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs';
+// @ts-ignore
+import {environment} from '@env/environment';
+import * as queryString from 'query-string';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  private url(url: string) {
+    return environment.apiUrl + url;
+  }
+
+  get(url: string, data: object = {}): Observable<any> {
+    if (Object.keys(data).length > 0) {
+      url += '?' + queryString.stringify(data);
+    }
+    return this.http.get(this.url(url));
+  }
+
+  post(url: string, data: object, options: object = {}): Observable<any> {
+    return this.http.post(this.url(url), data, options);
+  }
+
+  patch(url: string, data: object, options: object = {}): Observable<any> {
+    return this.http.patch(this.url(url), data, options);
+  }
+
+  delete(url: string): Observable<any> {
+    return this.http.delete(this.url(url));
+  }
+
+  put(url: string, data: object): Observable<any> {
+    return this.http.put(this.url(url), data);
+  }
+
+  request(method: string, url: string, data: object, options: object): Observable<any> {
+    const req = new HttpRequest(method, this.url(url), data, options);
+    return this.http.request(req);
+  }
+}
