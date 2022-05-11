4073f8df0df4642727750aa02224811f92e00c51
Pashimati <Pashimatii@gmail.com>
2022-05-11 20:48:43 +0300

    add not-found

---
+++ src/app/pages/not-found/not-found.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from "@angular/core";
+import {SharedModule} from "@shared/shared.module";
+import {NotFoundComponent} from "./not-found.component";
+import {NotFoundRoutingModule} from "./not-found-routing.module";
+
+
+
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ],
+  imports: [
+    SharedModule,
+    NotFoundRoutingModule
+  ],
+  exports: [NotFoundComponent],
+  providers: [],
+  bootstrap: [NotFoundComponent]
+})
+export class NotFoundModule { }
