cddaee960b4d45a35ae758df0f8fa6010aba5ae8
Pashimati <Pashimatii@gmail.com>
2022-05-18 15:40:57 +0300

    change avatar doctor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-medical-clinic.herokuapp.com/'
+  baseUrl: 'https://api-medical-clinic.herokuapp.com/',
+  firebase: {
+    apiKey: "AIzaSyAN8J6iltTJ7bTU-76hBSk8qK1E2OGBO_A",
+    authDomain: "medical-clinic-db.firebaseapp.com",
+    projectId: "medical-clinic-db",
+    storageBucket: "medical-clinic-db.appspot.com",
+    messagingSenderId: "529552749456",
+    appId: "1:529552749456:web:7b23b6f033079e01b83915",
+    measurementId: "G-5H64M71JY8"
+  }
 };
